9020c7cfaa7b64026aacdbbd029b983af029dad5
Ben Feng <fybmain@gmail.com>
2018-12-06 20:55:16 +0800

    Adding dormitory information updating
    Signed-off-by: Ben Feng <fybmain@gmail.com>

--- DormitoryBackend/src/dormitory.py
+++ DormitoryBackend/src/dormitory.py
@@ -1,8 +1,8 @@
 from math import ceil
 
-from .util import http
+from .util import http, get_request_json
 from .global_obj import app
-from .model import Dormitory
+from .model import Dormitory, Building, ElectricityMeter, WaterMeter
 from .permission import get_permission_condition, check_permission_condition
 
 
@@ -14,8 +14,8 @@ def generate_dormitory_info(dormitory: Dormitory):
             "id": dormitory.building_id,
             "name": dormitory.building.name,
         },
-        "electricity_meter_id": dormitory.electricity_meter_id,
-        "water_meter_id": dormitory.water_meter_id,
+        "electricity_meter": dormitory.electricity_meter_id,
+        "water_meter": dormitory.water_meter_id,
     }
 
 
@@ -40,3 +40,46 @@ def get_dormitory_by_id(id: int):
     dormitory = Dormitory.get(id=id)
     check_permission_condition(dormitory, get_permission_condition(["Management", "Self"], Dormitory))
     return http.Success(result=generate_dormitory_info(dormitory))
+
+
+@app.route("/dormitory/id/<int:id>", methods=["PUT"])
+def update_dormitory_info(id: int):
+    instance = get_request_json(schema={
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "type": "object",
+        "properties": {
+            "number": {
+                "type": "string",
+            },
+            "building": {
+                "type": "number",
+            },
+            "electricity_meter": {
+                "type": "number",
+            },
+            "water_meter": {
+                "type":"number",
+            },
+        },
+        "additionalProperties": False,
+    })
+    dormitory = Dormitory.get(id=id)
+    check_permission_condition(dormitory, get_permission_condition(["Management"], Dormitory))
+
+    if "building" in instance:
+        building_id = instance["building"]
+        building = Building.get(id=building_id)
+
+    if "electricity_meter" in instance:
+        electricity_meter_id = instance["electricity_meter"]
+        electricity_meter = ElectricityMeter.get(id=electricity_meter_id)
+
+    if "water_meter" in instance:
+        water_meter_id = instance["water_meter"]
+        water_meter = WaterMeter.get(id=water_meter_id)
+
+    for (key, value) in instance.items():
+        setattr(dormitory, key, value)
+
+    dormitory.save()
+    return http.Success()
