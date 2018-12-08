5da0b02b02d8e4a36dd9c2389f09db9bd0d8e340
Ben Feng <fybmain@gmail.com>
2018-12-09 01:11:31 +0800

    Adding create,retrieve,update handlers of dormitory
    Signed-off-by: Ben Feng <fybmain@gmail.com>

--- DormitoryBackend/src/dormitory.py
+++ DormitoryBackend/src/dormitory.py
@@ -1,9 +1,41 @@
-from math import ceil
+from typing import List
 
-from .util import http, get_request_json
+from .util import http, get_request_json, get_filter_condition, generate_pagination_list
 from .global_obj import app
 from .model import Dormitory, Building, ElectricityMeter, WaterMeter
-from .permission import get_permission_condition, check_permission_condition
+from .permission import get_permission_condition, check_permission_condition, PermissionDenied
+
+
+dormitory_normal_properties = {
+    "number": {
+        "type": "string",
+        "pattern": "^[0-9]+$",
+    },
+    "building": {
+        "type": "number",
+    },
+    "electricity_meter": {
+        "type": "number",
+    },
+    "water_meter": {
+        "type": "number",
+    },
+}
+
+
+dormitory_filter_properties = dict(dormitory_normal_properties, id={
+    "type": "number",
+})
+
+
+dormitory_updatable_properties = dict(dormitory_normal_properties)
+
+
+def get_dormitories(filter: dict, allowed: List[str]):
+    return Dormitory.select().where(
+        get_filter_condition(filter, Dormitory)
+        & get_permission_condition(allowed, Dormitory)
+    )
 
 
 def generate_dormitory_info(dormitory: Dormitory):
@@ -19,66 +51,111 @@ def generate_dormitory_info(dormitory: Dormitory):
     }
 
 
-@app.route("/dormitory/list/page/<int:page_num>", methods=["GET"])
-def get_dormitory_list(page_num: int):
-    dormitories = (
-            Dormitory
-            .select()
-            .where(get_permission_condition(["Management", "Self"], Dormitory))
-    )
-    dormitory_count = dormitories.count()
-    dormitory_list = dormitories.paginate(page_num)
-    instance_list = [generate_dormitory_info(dormitory) for dormitory in dormitory_list]
-    return http.Success(result={
-        "page_count": int(ceil(dormitory_count/20)),
-        "list": instance_list,
-    })
-
-
-@app.route("/dormitory/id/<int:id>", methods=["GET"])
-def get_dormitory_by_id(id: int):
-    dormitory = Dormitory.get(id=id)
-    check_permission_condition(dormitory, get_permission_condition(["Management", "Self"], Dormitory))
-    return http.Success(result=generate_dormitory_info(dormitory))
-
-
-@app.route("/dormitory/id/<int:id>", methods=["PUT"])
-def update_dormitory_info(id: int):
+@app.route("/dormitory/list", methods=["POST"])
+def get_dormitory_list():
     instance = get_request_json(schema={
         "$schema": "http://json-schema.org/draft-07/schema#",
         "type": "object",
         "properties": {
-            "number": {
-                "type": "string",
-            },
-            "building": {
+            "page": {
                 "type": "number",
             },
-            "electricity_meter": {
+            "limit": {
                 "type": "number",
             },
-            "water_meter": {
-                "type":"number",
+            "filter": {
+                "type": "object",
+                "properties": dormitory_filter_properties,
+                "additionalProperties": False,
             },
         },
+        "required": ["page", "limit", "filter"],
         "additionalProperties": False,
     })
-    dormitory = Dormitory.get(id=id)
-    check_permission_condition(dormitory, get_permission_condition(["Management"], Dormitory))
+    dormitories = get_dormitories(instance["filter"], ["Management", "Self"])
+    return http.Success(result=generate_pagination_list(
+        objs=dormitories,
+        instance_generator=generate_dormitory_info,
+        page=instance["page"],
+        limit=instance["limit"]
+    ))
 
-    if "building" in instance:
-        building_id = instance["building"]
+
+def obj_process(obj: dict):
+    if "building" in obj:
+        building_id = obj["building"]
         building = Building.get(id=building_id)
+        check_permission_condition(building, get_permission_condition(["Management"], Building))
 
-    if "electricity_meter" in instance:
-        electricity_meter_id = instance["electricity_meter"]
+    if "electricity_meter" in obj:
+        electricity_meter_id = obj["electricity_meter"]
         electricity_meter = ElectricityMeter.get(id=electricity_meter_id)
+        check_permission_condition(electricity_meter, get_permission_condition(["Management"], ElectricityMeter))
 
-    if "water_meter" in instance:
-        water_meter_id = instance["water_meter"]
+    if "water_meter" in obj:
+        water_meter_id = obj["water_meter"]
         water_meter = WaterMeter.get(id=water_meter_id)
+        check_permission_condition(water_meter, get_permission_condition(["Management"], WaterMeter))
 
-    for (key, value) in instance.items():
+
+@app.route("/dormitory/update", methods=["POST"])
+def update_dormitory_info():
+    instance = get_request_json(schema={
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "type": "object",
+        "properties": {
+            "filter": {
+                "type": "object",
+                "properties": dormitory_filter_properties,
+                "additionalProperties": False,
+            },
+            "obj": {
+                "type": "object",
+                "properties": dormitory_updatable_properties,
+                "additionalProperties": False,
+            },
+        },
+        "required": ["filter", "obj"],
+        "additionalProperties": False,
+    })
+    obj_process(instance["obj"])
+
+    allow_read_dormitory = get_dormitories(instance["filter"], ["Management", "Self"])
+    if allow_read_dormitory.count() < 1:
+        raise Dormitory.DoesNotExist()
+
+    allow_write_dormitory = get_dormitories(instance["filter"], ["Management"])
+    if allow_write_dormitory.count() < 1:
+        raise PermissionDenied()
+
+    for dormitory in allow_write_dormitory:
+        for(key, value) in instance["obj"].items():
+            setattr(dormitory, key, value)
+        dormitory.save()
+
+    return http.Success()
+
+
+@app.route("/dormitory/create", methods=["POST"])
+def create_dormitory():
+    instance = get_request_json(schema={
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "type": "object",
+        "properties": {
+            "obj": {
+                "type": "object",
+                "properties": dormitory_updatable_properties,
+                "required": list(dormitory_updatable_properties.keys()),
+                "additionalProperties": False,
+            },
+        },
+        "required": ["obj"],
+        "additionalProperties": False,
+    })
+    obj_process(instance["obj"])
+
+    dormitory = Dormitory()
+    for (key, value) in instance["obj"].items():
         setattr(dormitory, key, value)
 
     dormitory.save()
